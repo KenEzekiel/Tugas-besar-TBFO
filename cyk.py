b2699435781f469a87d56291409d0a0ef19f7253
arsaizdihar <arsadihar@gmail.com>
2022-11-13 11:14:47 +0700

    Add function cfg

--- cyk.py
+++ cyk.py
@@ -2,58 +2,82 @@ import time
 from cnf import CNF
 from pprint import pprint
 import inputreader
+import re
+
+
+class CYK:
+    def __init__(self, filename):
+        self.load_cnf(filename)
+
+    def load_cnf(self, filename):
+        cnf = CNF()
+        cnf.load(filename)
+        self.rules = cnf.rules
+        self.start = cnf.start
+
+    def check(self, words):
+        n = len(words)
+        if n == 0:
+            return True
+
+        # Inisialisasi Tabel CYK
+        Back = [[set([]) for j in range(n)] for i in range(n)]
+
+        # Isi Tabel CYK
+        for j in range(0, n):
+            # Iterasi Production Rules
+            for left, rule in self.rules.items():
+                for right in rule:
+
+                    # Saat ditemukan terminal
+                    if len(right) == 1 and right[0] == words[j]:
+                        Back[j][j].add(left)
+
+            for i in range(j, -1, -1):
+
+                # Iterasi dari i sampai j + 1
+                for k in range(i, j + 1):
+                    if k + 1 >= n or len(Back[i][k]) == 0 or len(Back[k + 1][j]) == 0:
+                        continue
+
+                    # Iterasi Production Rules
+                    for left, rule in self.rules.items():
+                        for right in rule:
+
+                            # Saat terminal ditemukan
+                            if len(right) == 2 and right[0] in Back[i][k] and right[1] in Back[k + 1][j]:
+                                Back[i][j].add(left)
+        # pprint(Back)
+        # Cek apakah Start Rule S ada di indeks o, n-1 atau tidak
+        return self.start in (Back[0][n-1])
+
+
+def function_check(w: str):
+    with open("./function_terminals.txt", 'r') as a:
+        function_terminals = a.read().split()
+    func_cyk = CYK("func_cnf.txt")
+    func_words = inputreader.inputread(w, function_terminals)
+    if not func_cyk.check(func_words):
+        print("Not a valid function")
+        exit()
+
+
+with open("input.txt", 'r') as f:
+    w = f.read()
 
 with open("./terminals.txt", 'r') as a:
     terminals = a.read().split()
-with open("./variables.txt", 'r') as b:
-    non_terminals = b.read().split()
 
-cnf = CNF()
-cnf.load('cnf.txt')
-# Grammar dari CNF
-R = cnf.rules
-start = cnf.start
+function_check(w)
 
+terminals.append(' ')
+terminals.append(r'\n')
+cyk = CYK("cnf.txt")
 
-# Function to perform the CYK Algorithm
-def CYKCheck(w):
-    n = len(w)
-
-    # Inisialisasi Tabel CYK
-    Back = [[set([]) for j in range(n)] for i in range(n)]
-
-    # Isi Tabel CYK
-    for j in range(0, n):
-
-        # Iterasi Production Rules
-        for left, rule in R.items():
-            for right in rule:
-
-                # Saat ditemukan terminal
-                if len(right) == 1 and right[0] == w[j]:
-                    Back[j][j].add(left)
-
-        for i in range(j, -1, -1):
-
-            # Iterasi dari i sampai j + 1
-            for k in range(i, j + 1):
-                if k + 1 >= n or len(Back[i][k]) == 0 or len(Back[k + 1][j]) == 0:
-                    continue
-
-                # Iterasi Production Rules
-                for left, rule in R.items():
-                    for right in rule:
-
-                        # Saat terminal ditemukan
-                        if len(right) == 2 and right[0] in Back[i][k] and right[1] in Back[k + 1][j]:
-                            Back[i][j].add(left)
-    # pprint(Back)
-    # Cek apakah Start Rule S ada di indeks o, n-1 atau tidak
-    if start in (Back[0][n-1]):
-        print("True")
-    else:
-        print("False")
-
-
-w = inputreader.inputread("input.txt")
-CYKCheck(w)
+w = inputreader.preprocessing(w)
+words = inputreader.inputread(w, terminals)
+valid = cyk.check(words)
+if valid:
+    print("True")
+else:
+    print("False")
--- inputreader.py
+++ inputreader.py
@@ -1,22 +1,12 @@
 import pprint
 import re
+from fa import check_fa
 
-
-with open("./terminals.txt", 'r') as a:
-    terminals = a.read().split()
-with open("./variables.txt", 'r') as b:
-    non_terminals = b.read().split()
 
 space_reg = r'((?:return)|(?:var)|(?:let)|(?:const)|(?:delete)|(?:function)|(?:case))(?:\s+)((?:variable)|(?:mathexp))'
 
 
-def inputread(filename):
-    with open("./" + filename, 'r') as f:
-        w = f.read()
-    if re.match(r"^\s*$", w):
-        return [" "]
-
-    # preprocessing
+def preprocessing(w: str):
     w = re.sub(r"else\s+if", "elsexxif", w)
     w = re.sub(space_reg, r'\1xx\2', w)
     w = re.sub(r'comment\n', 'commentwww', w)
@@ -24,22 +14,17 @@ def inputread(filename):
     w = w.replace('xx', ' ')
     w = w.replace('www', '\n')
 
-    terminals.append(' ')
-    terminals.append(r'\n')
+    return w
+
+
+def inputread(w: str, terminals: list):
+    # if re.match(r"^\s*$", w):
+    #     return [" "]
+
     regex = "|".join(map(lambda x: "(" + x + ")", terminals))
-    w = re.sub(r'[ \t]+', ' ', w)
 
     iter = re.finditer(regex, w)
     parsed = list(map(lambda x: x.group(0), iter))
 
     # print(parsed)
-
-    new_parsed = []
-    for i in parsed:
-        if i == " ":
-            i = "space"
-        new_parsed.append(i)
-
-    # print(new_parsed)
-
     return parsed
