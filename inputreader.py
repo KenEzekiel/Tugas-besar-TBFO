58e9bd8f87706e2a107e7f40895b2a4acfd037ea
arsaizdihar <arsadihar@gmail.com>
2022-11-25 22:37:05 +0700

    Add some edge cases handle

--- inputreader.py
+++ inputreader.py
@@ -2,7 +2,7 @@ import pprint
 import re
 
 # list of place that space is obligatory
-space_reg = r'((?:return)|(?:var)|(?:let)|(?:const)|(?:delete)|(?:function)|(?:case))(?:\s+)((?:[()]*variable)|(?:[()]*number))'
+space_reg = r'((?:return)|(?:var)|(?:let)|(?:const)|(?:delete)|(?:function)|(?:case))(?:\s+)((?:[({\[]*\s*variable)|(?:[({\[]*\s*number))'
 
 
 def preprocessing(w: str):
